0382aee0aac90f94fcff83d46508da488ca7704b
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2022-04-19 18:14:25 -0500

    Almost finished the math quiz console app, but still need to figure out how to print the final quiz score.

--- Playground/MathQuiz/Program.cs
+++ Playground/MathQuiz/Program.cs
@@ -1,32 +1,61 @@
 ﻿Console.WriteLine("Welcome to the square-roots- and exponents-level Math Quiz. Are you ready to take the test? You will have five seconds to close this console window.");
 Thread.Sleep(5000);
-int i = 0;
-while (i < 15)
+
+Console.WriteLine("First question.");
+Console.WriteLine("What is the square root of 81?");
+   if (Console.ReadLine() == "9")
+   {
+        int a = 20;
+        Console.WriteLine("Correct.");
+   }
+   else
+   {
+        Console.WriteLine("Incorrect.");
+   }
+
+Console.WriteLine("What is 4 to the sixth power?");
+   if (Console.ReadLine() == "4096")
+   {
+        Console.WriteLine("Correct.");
+        int b = 40;
+   }
+   else
+   {
+        Console.WriteLine("Incorrect.");
+   }
+
+Console.WriteLine("What is the square root of 49 times 3^3?");
+   if (Console.ReadLine() == "189")
+   {
+        Console.WriteLine("Correct.");
+        int c = 60;
+   }
+   else
+   {
+        Console.WriteLine("Incorrect.");
+   }
+
+Console.WriteLine("What is 4^5 times 2^8?");
+   if (Console.ReadLine() == "262144")
+   {
+        Console.WriteLine("Correct.");
+        int d = 80;
+   }
+   else
+   {
+        Console.WriteLine("Incorrect.");
+   }
+
+Console.WriteLine("Final question: what is 3^4 times 4^3?");
 {
-    i += 10;
-    Console.WriteLine("First question.");
-    Console.WriteLine("What is the square root of 81?");
-    if (Console.ReadLine() == "9")
-    {
+   if (Console.ReadLine() == "5184")
+   {
         Console.WriteLine("Correct.");
-    }
-
-    else
-    {
+        int e = 100;
+   }
+   else
+   {
         Console.WriteLine("Incorrect.");
-
-    }
-
-    Console.WriteLine("What is 4 to the sixth power?");
-    if (Console.ReadLine() == "4096")
-    {
-        Console.WriteLine("Correct.");
-    }
-
-    else
-    {
-        Console.WriteLine("Incorrect.");
-    }
-    continue;
+   }
 }
-Console.WriteLine(i);
+Console.WriteLine("Thank you for completing this math quiz. Your score is"); // Figure out how to print the score using the 5 variables a, b, c, d, and e above.
