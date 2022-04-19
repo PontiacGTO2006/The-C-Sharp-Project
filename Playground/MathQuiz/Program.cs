45193659b2c769e0edd8344e0bbf4cb7f5f8c581
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2022-04-19 13:19:51 -0500

    Continued on the Math Quiz console app. Still trying to figure out how to track the user's score.

--- Playground/MathQuiz/Program.cs
+++ Playground/MathQuiz/Program.cs
@@ -1,2 +1,32 @@
-﻿Console.WriteLine("Welcome to the square-roots level Math Quiz. Are you ready to take the test? You will have five seconds to close this console window.");
+﻿Console.WriteLine("Welcome to the square-roots- and exponents-level Math Quiz. Are you ready to take the test? You will have five seconds to close this console window.");
 Thread.Sleep(5000);
+int i = 0;
+while (i < 15)
+{
+    i += 10;
+    Console.WriteLine("First question.");
+    Console.WriteLine("What is the square root of 81?");
+    if (Console.ReadLine() == "9")
+    {
+        Console.WriteLine("Correct.");
+    }
+
+    else
+    {
+        Console.WriteLine("Incorrect.");
+
+    }
+
+    Console.WriteLine("What is 4 to the sixth power?");
+    if (Console.ReadLine() == "4096")
+    {
+        Console.WriteLine("Correct.");
+    }
+
+    else
+    {
+        Console.WriteLine("Incorrect.");
+    }
+    continue;
+}
+Console.WriteLine(i);
