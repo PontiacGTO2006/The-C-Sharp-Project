bc40de3e65b029fec41769cf9021ab8295905c2b
PontiacGTO2006 <101056722+PontiacGTO2006@users.noreply.github.com>
2022-06-12 08:50:27 -0500

    Officially "got rid of" the Math Quiz. Will move to a different console application idea in the future.

--- Playground/MathQuiz/Program.cs
+++ Playground/MathQuiz/Program.cs
@@ -1,67 +1,67 @@
-﻿Console.WriteLine("Welcome to the square-roots- and exponents-level Math Quiz. Are you ready to take the test? You will have five seconds to close this console window.");
-// Thread.Sleep(5000);
+﻿//Console.WriteLine("Welcome to the square-roots- and exponents-level Math Quiz. Are you ready to take the test? You will have five seconds to close this console window.");
+//// Thread.Sleep(5000);
 
-Console.WriteLine("First question.");
-Console.WriteLine("What is the square root of 81?");
-   if (Console.ReadLine() == "9")
-   {
-        Console.WriteLine("Correct.");
-        int v = 20;
-}
-   else
-   {
-        Console.WriteLine("Incorrect.");
-   }
+//Console.WriteLine("First question.");
+//Console.WriteLine("What is the square root of 81?");
+//   if (Console.ReadLine() == "9")
+//   {
+//        Console.WriteLine("Correct.");
+//        Console.WriteLine("Your score is 1.");
+//        bool answer = true;
+//}
+//   else
+//   {
+//        Console.WriteLine("Incorrect.");
+//        Console.WriteLine("Your score is 0.");
+//        bool answer = false;
+//   }
+//Console.WriteLine("What is 4 to the sixth power?");
+//   if (Console.ReadLine() == "4096")
+//   {
+//        Console.WriteLine("Correct.");
+//        bool answer;
+//        if (answer = false)
+//        {
+//            Console.WriteLine("Your score is 1.");
+//        }
+//        Console.WriteLine("Your score is 2");
+//   }
+//   else
+//   {
+//        Console.WriteLine("Incorrect.");
+//   }
 
-Console.WriteLine("What is 4 to the sixth power?");
-   if (Console.ReadLine() == "4096")
-   {
-        Console.WriteLine("Correct.");
-        int v = 20;
-        int a = v + 20;
-   }
-   else
-   {
-        Console.WriteLine("Incorrect.");
-   }
+//Console.WriteLine("What is the square root of 49 times 3^3?");
+//   if (Console.ReadLine() == "189")
+//   {
+//        Console.WriteLine("Correct.");
+//   }
+//   else
+//   {
+//        Console.WriteLine("Incorrect.");
+//   }
 
-Console.WriteLine("What is the square root of 49 times 3^3?");
-   if (Console.ReadLine() == "189")
-   {
-        Console.WriteLine("Correct.");
-        int v = 40;
-        int b = v + 20;
-   }
-   else
-   {
-        Console.WriteLine("Incorrect.");
-   }
+//Console.WriteLine("What is 4^5 times 2^8?");
+//   if (Console.ReadLine() == "262144")
+//   {
+//        Console.WriteLine("Correct.");
+//        var score4 = 4;
+//   }
+//   else
+//   {
+//        Console.WriteLine("Incorrect.");
+//   }
 
-Console.WriteLine("What is 4^5 times 2^8?");
-   if (Console.ReadLine() == "262144")
-   {
-        Console.WriteLine("Correct.");
-        int v = 60;
-        int c = v + 20;
-   }
-   else
-   {
-        Console.WriteLine("Incorrect.");
-   }
-
-Console.WriteLine("Final question: what is 3^4 times 4^3?");
-{
-   if (Console.ReadLine() == "5184")
-   {
-        Console.WriteLine("Correct.");
-        int v = 80; 
-        int d = v + 20;
-        Console.WriteLine(d);
-    }
-   else
-   {
-        Console.WriteLine("Incorrect.");
-        Console.WriteLine(d);
-    }
-}
-Console.WriteLine("Thank you for completing this math quiz. Your score is"); // Figure out how to print the score using the 5 variables a, b, c, d, and e above.
+//Console.WriteLine("Final question: what is 3^4 times 4^3?");
+//{
+//   if (Console.ReadLine() == "5184")
+//   {
+//        Console.WriteLine("Correct.");
+//        var score5 = 5;
+//    }
+//   else
+//   {
+//        Console.WriteLine("Incorrect.");
+//    }
+//}
+//Console.WriteLine("Thank you for completing this math quiz. Your score is"); // Figure out how to print the score using the 5 variables a, b, c, d, and e above.
